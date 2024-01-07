fdb1ffce92061656177fb938d6c7c3e816c94922
shaunkh <shaun.khundker@gmail.com>
2024-01-07 11:40:40 +0600

    pig

--- project1.py
+++ project1.py
@@ -10,7 +10,32 @@ while True:
   else: 
     print("Pick a number from 2 to 4. Please try again.")
 
-max_score = 0
+max_score = 50
 player_scores = [0 for _ in range(int(players))]
 
-while max_score < max(player_scores)
+while max_score > max(player_scores):
+  for i in range(int(players)):
+    print("Player " + str(i + 1) + "'s turn")
+    score = 0
+
+    while True:
+      should_roll = input("Would you like to roll? (y/n) ")
+      if(should_roll.lower() != "y"):
+        break
+
+      value = roll()
+      print("You rolled a", value)
+      if(value == 1):
+        print("Turn ended!")
+        score = 0
+        break
+      else: 
+        score += value
+        
+      print("Your score is", score)
+
+    player_scores[i] += score
+    print("Your total score is", player_scores[i])
+
+  
+
